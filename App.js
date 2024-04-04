555feff28d62648f22fa784c6f98a7ddab47a03e
jungbee2 <126428632+jungbee2@users.noreply.github.com>
2024-04-04 16:19:26 +0900

    dD

--- App.js
+++ App.js
@@ -1,11 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
 import {
   SafeAreaView,
@@ -15,49 +7,93 @@ import {
   Text,
   Pressable,
   View,
+  Image,
+  TextInput,
 } from 'react-native';
 
-
 const App = () => {
+
   return (
-    <View>
-      <View >
-        <Text >
-          Spiriataaasdasdadsadasdasdasdasdasd
-        </Text>
+
+    <View style={styles.container}>
+      <View style={styles.box1}>
+        <Image
+          source={require('././assets/spiritLogo.png')}
+          style={styles.imageStyle}
+        />
       </View>
       <View>
-      <Pressable>
-      <Text>로그인1asd</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="전화번호"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="비밀번호" 
+        />
+      </View>
+      <View style={styles.linksContainer}>
+      <Text style={styles.link}>회원가입</Text>
+      <Text style={styles.separator}>| </Text>
+        <Text style={styles.link}>비밀번호 찾기</Text>
+      </View>
+      <View>
+        <Pressable style={styles.LoginButton}>
+          <Text>로그인</Text>
         </Pressable>
-    </View>
-    <View>
-    <Pressable style={styles.LoginButton}>
-    <Text>로그인2</Text>
-        </Pressable>
-    </View>
-    <View>
-    <Pressable>
-        <Text>로그인3</Text>
-        </Pressable>
-    </View>
+      </View>
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop:100,
-    padding:100
+  linksContainer: {
+    marginTop:30,
+    flexDirection: 'row', // 수평 방향으로 요소들을 배치
+    alignItems: 'center', 
+    justifyContent: 'center',
+  },
+  link: {
+    textAlign:'center',
+    paddingLeft:50, 
+    paddingRight:50, 
+  },
+  separator: {
+    paddingVertical: 10,
+  },
+  input:{
+    marginLeft:50,
+    marginRight:50,
+    marginBottom: 20,
+    height: 40,
+    borderColor: '#ced4da',
+    borderBottomWidth: 1,
+    paddingHorizontal: 10,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  box1: {
+    height: 330,
+    padding: 20,
+    justifyContent: 'center', // 세로 방향 가운데 정렬
+    alignItems: 'center', // 가로 방향 가운데 정렬
   },
   LoginButton: {
-    backgroundColor: '#B6BE6A',
+    backgroundColor: '#FFFFFF',
+    borderWidth: 2, // 테두리 두께
+    borderColor: '#80D2F4', // 테두리 색상
     paddingHorizontal: 110,
     paddingVertical: 10,
-    margin:20,
-    alignItems:'center',
+    margin: 50,
+    alignItems: 'center',
     borderRadius: 10,
     marginBottom: 10,
+  },
+  imageStyle: {
+    width: 130,
+    resizeMode: 'contain',
   },
   sectionTitle: {
     fontSize: 24,
@@ -72,5 +108,6 @@ const styles = StyleSheet.create({
     fontWeight: '700',
   },
 });
+
 
 export default App;
